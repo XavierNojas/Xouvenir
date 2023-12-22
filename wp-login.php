75fd59a5cbf44e81069552a869dcee19d91e1a98
XavierNojas <77745249+XavierNojas@users.noreply.github.com>
2023-12-22 16:12:11 +0800

    Update 2024

---
+++ wp-login.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en-US" prefix="og: https://ogp.me/ns#">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Log In &lsaquo; Xouvenir Studios &#8212; WordPress</title>
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="dns-prefetch" href="//use.fontawesome.com">
+<link rel="stylesheet" id="qodeblock-fontawesome-css" href="https://xaviernojas.github.io/Xouvenir/wp-content/plugins/qodeblock/dist/assets/fontawesome/css/all.min.css?ver=1615054292" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://xaviernojas.github.io/Xouvenir/wp-includes/css/dashicons.min.css?ver=6.4.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://xaviernojas.github.io/Xouvenir/wp-includes/css/buttons.min.css?ver=6.4.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://xaviernojas.github.io/Xouvenir/wp-admin/css/forms.min.css?ver=6.4.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://xaviernojas.github.io/Xouvenir/wp-admin/css/l10n.min.css?ver=6.4.2" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://xaviernojas.github.io/Xouvenir/wp-admin/css/login.min.css?ver=6.4.2" type="text/css" media="all">
+<link rel="stylesheet" id="font-awesome-official-css" href="https://use.fontawesome.com/releases/v6.4.0/css/all.css" type="text/css" media="all" integrity="sha384-iw3OoTErCYJJB9mCa8LNS2hbsQ7M3C0EpIsO/H5+EGAkPGc6rk+V8i04oW/K5xq0" crossorigin="anonymous">
+<link rel="stylesheet" id="font-awesome-official-v4shim-css" href="https://use.fontawesome.com/releases/v6.4.0/css/v4-shims.css" type="text/css" media="all" integrity="sha384-TjXU13dTMPo+5ZlOUI1IGXvpmajjoetPqbUJqTx+uZ1bGwylKHNEItuVe/mg/H6l" crossorigin="anonymous">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+	<link rel="icon" href="https://xaviernojas.github.io/Xouvenir/wp-content/uploads/2023/07/cropped-Xouvenir-Studios-BLACK-1-32x32.png" sizes="32x32">
+<link rel="icon" href="https://xaviernojas.github.io/Xouvenir/wp-content/uploads/2023/07/cropped-Xouvenir-Studios-BLACK-1-192x192.png" sizes="192x192">
+<link rel="apple-touch-icon" href="https://xaviernojas.github.io/Xouvenir/wp-content/uploads/2023/07/cropped-Xouvenir-Studios-BLACK-1-180x180.png">
+<meta name="msapplication-TileImage" content="https://xaviernojas.github.io/Xouvenir/wp-content/uploads/2023/07/cropped-Xouvenir-Studios-BLACK-1-270x270.png">
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">/*  */
+document.body.className = document.body.className.replace('no-js','js');
+/*  */</script>
+
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://localhost/wordpress/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username" required="required">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+									<input type="hidden" name="redirect_to" value="http://localhost/wordpress/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+				<a class="wp-login-lost-password" href="https://xaviernojas.github.io/Xouvenir/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
+			<script type="text/javascript">/*  */
+function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }
+/*  */</script>
+		<p id="backtoblog">
+			<a href="https://xaviernojas.github.io/Xouvenir/">&larr; Go to Xouvenir Studios</a>		</p>
+			</div>
+			<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-content/themes/reflector/assets/js/lib/jquery-migrate.min.js?ver=1.4.1-wp" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">/*  */
+var _zxcvbnSettings = {"src":"https:\/\/xaviernojas.github.io\/Xouvenir\/wp-includes\/js\/zxcvbn.min.js"};
+/*  */</script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.14.0" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-includes/js/dist/hooks.min.js?ver=c6aec9a8d4e5a5d543a1" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-includes/js/dist/i18n.min.js?ver=7701b0c3857f914212ef" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
+/*  */
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+/*  */
+</script>
+<script type="text/javascript" id="password-strength-meter-js-extra">/*  */
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/*  */</script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-admin/js/password-strength-meter.min.js?ver=6.4.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">/*  */
+var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
+/*  */</script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-includes/js/wp-util.min.js?ver=6.4.2" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">/*  */
+var userProfileL10n = {"user_id":"0","nonce":"195f76414f"};
+/*  */</script>
+<script type="text/javascript" src="https://xaviernojas.github.io/Xouvenir/wp-admin/js/user-profile.min.js?ver=6.4.2" id="user-profile-js"></script>
+	</body>
+	</html>
